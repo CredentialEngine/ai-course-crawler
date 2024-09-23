2af9dd6dd7af872ce36375282bc9ea7274bb8198
Rômulo Saksida <romulo@rsaksida.com>
2024-09-23 12:13:28 -0300

    Update entrypoint script

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ ! -f "$DATABASE_URL" ]; then
-  echo "Creating database"
-  ./node_modules/.bin/drizzle-kit generate:sqlite
-fi
 echo "Running migrations"
-node dist/migrate.js
+./node_modules/.bin/drizzle-kit migrate
 
 chown -R pptruser:pptruser /app
 chown -R pptruser:pptruser /app/db
